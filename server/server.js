893866821a92c81f425fe791717d513c7cea3d93
Abdul <abdulrahman.alsumaih@icloud.com>
2021-03-23 20:14:41 -0400

    edit server.js

--- server/server.js
+++ server/server.js
@@ -2,3 +2,18 @@ import bodyParser from 'body-parser';
 import exporess from 'express';
 import Mongoose from 'mongoose';
 import cors from 'cors';
+
+
+// connect to database
+mongoose.connect(
+    database,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+)
+.then(() => console.log("Connected to database successfully"))
+.catch(err => console.log(err));
+
+
+
